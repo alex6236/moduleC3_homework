9df61a7a85fc912f55403ed3fffcad03042495e1
Alex <sova11@mail.ru>
2023-04-23 10:55:23 +0300

    no comments

--- C3.3_task1.js
+++ C3.3_task1.js
@@ -1,28 +1,28 @@
 const generalObj = {
-    one: '1-Главный объект',
-    two: '2-Главный объект',
-    three: '3-Главный объект'
+  one: '1-Главный объект',
+  two: '2-Главный объект',
+  three: '3-Главный объект'
 }
 
-const onwObj = Object.create(generalObj);
-    onwObj.ownOne = '1-Собственный объект';
-    onwObj.ownTwo = '2-Собственный объект';
-    onwObj.ownThree = '3-Собственный объект';
+const onwObj = Object.create(generalObj)
+onwObj.ownOne = '1-Собственный объект'
+onwObj.ownTwo = '2-Собственный объект'
+onwObj.ownThree = '3-Собственный объект'
 
 // --------------------------------
 // Как вариант
 
 for (const [key, value] of Object.entries(onwObj)) {
-    console.log(`${key} ${value}`);
+  console.log(`${key} ${value}`)
 }
 // ----------------------------------
 
 function printObject (obj) {
-    for (let prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
-            console.log(prop, obj[prop]);
-        }
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      console.log(prop, obj[prop])
     }
+  }
 }
 
 // function printObject (obj) {
@@ -31,4 +31,4 @@ function printObject (obj) {
 //     }
 // }
 
-printObject(onwObj);
+printObject(onwObj)
